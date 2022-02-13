01f78078a3349ca38fef6fa8616a1e6983535d79
camiloJTG <camilojtg.devep@protonmail.com>
2022-02-13 17:06:22 -0300

    feat: Add type in function

--- src/utils/response.service.ts
+++ src/utils/response.service.ts
@@ -2,14 +2,14 @@ import { Injectable, HttpException } from '@nestjs/common';
 import { successResponse } from './interfaces/response.interface';
 @Injectable()
 export class ResponseService {
-  successResponse(statusCode: number, message: {}) {
+  successResponse(statusCode: number, message: {}): successResponse {
     const result: successResponse = {
       statusCode,
       message,
     };
     return result;
   }
-  exceptionResponse(httpStatus: number, message: string) {
+  exceptionResponse(httpStatus: number, message: string): HttpException {
     throw new HttpException(message, httpStatus);
   }
 }
